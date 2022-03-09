16964c6e1a3b3226ebbc9d0750360f540a331a0b
Oganesian, Tigran <oganetig@fel.cvut.cz>
2022-03-09 18:59:09 +0100

    update 1st homework

--- homeworks/hw01/src/main/java/cz/cvut/fel/ts1/Math.java
+++ homeworks/hw01/src/main/java/cz/cvut/fel/ts1/Math.java
@@ -6,6 +6,15 @@ public class Math {
         System.out.println("result for 3! is " + factorialRecursive(3));
     }
 
+    public int factorialIterative(int n) {
+        int result = 1, i = 1;
+        while (i <= n) {
+            result = result * i;
+            i++;
+        }
+        return result;
+    }
+
     public static long factorialRecursive(int num) {
         if (num <= 1) return 1;
         return num * factorialRecursive(num - 1);
