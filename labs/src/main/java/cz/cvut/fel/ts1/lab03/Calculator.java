7f40203e08222a85e78601e689a70c8e5aeb6896
klimoval <klimoval@fel.cvut.cz>
2022-03-13 23:09:28 +0100

    update lab03

--- labs/src/main/java/cz/cvut/fel/ts1/lab03/Calculator.java
+++ labs/src/main/java/cz/cvut/fel/ts1/lab03/Calculator.java
@@ -11,8 +11,8 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
-    public int divide(int a, int b) {
-        if (b != 0) return a / b;
-        return 0;
+    public int divide(int a, int b) throws Exception {
+        if (b == 0) throw new Exception("Division by 0 is not allowed!");
+        return  a / b;
     }
 }
